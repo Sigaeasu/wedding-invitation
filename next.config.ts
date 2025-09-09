184afbb07fd688333b6a1fd62907bb3d2709653e
Sigi Kemahduta <sigikemahduta@gmail.com>
2025-09-09 15:01:12 +0700

    setup gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/wedding-invitation",
-  assetPrefix: "/wedding-invitation/",
+  basePath: isProd ? "/wedding-invitation" : "",
+  assetPrefix: isProd ? "/wedding-invitation/" : "",
   images: {
     unoptimized: true,
   },
